ee2361622544f898e0ad1c33180dbfbd6003a47e
Natawin Techawatcharapaikul <107763293+Natawin@users.noreply.github.com>
2025-06-12 14:21:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ else:
             output = model(img_tensor, valve_idx_tensor)
             prob = torch.sigmoid(output).item()
 
-        if prob > 0.5:
+        if prob > 0.7:
             st.error(f"❌ Abnormal")
         else:
             st.success(f"✅ Normal")
