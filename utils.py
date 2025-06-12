b521603bb5f95b6cf5572a95a1ae6f1bf8c0b14f
Natawin Techawatcharapaikul <107763293+Natawin@users.noreply.github.com>
2025-06-12 14:50:21 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import librosa
+import librosa.display
 import numpy as np
 from PIL import Image
-import cv2
 import matplotlib.pyplot as plt
 
 def generate_mel_image_for_model(wav_path):
@@ -9,9 +9,9 @@ def generate_mel_image_for_model(wav_path):
     S = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=1024, hop_length=256, n_mels=128)
     S_dB = librosa.power_to_db(S, ref=np.max)
     S_norm = (S_dB + 80) / 80
-    resized = cv2.resize(S_norm, (224, 224), interpolation=cv2.INTER_CUBIC)
+    resized = Image.fromarray((S_norm * 255).astype(np.uint8)).resize((224, 224), Image.BICUBIC)
     image = np.stack([resized, resized, resized], axis=-1)
-    image = (image * 255).astype(np.uint8)
+    image = np.array(image).astype(np.uint8)
     return Image.fromarray(image)
 
 def plot_mel_spectrogram(wav_path):
