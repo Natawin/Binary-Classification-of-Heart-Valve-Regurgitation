540b6bf50674994c886fc42b0634992d7ca2f805
Natawin Techawatcharapaikul <107763293+Natawin@users.noreply.github.com>
2025-06-12 11:04:18 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import librosa
 import numpy as np
 from PIL import Image
-import cv2
 
 def generate_mel_image(wav_path):
     y, sr = librosa.load(wav_path, sr=None)
@@ -9,7 +8,11 @@ def generate_mel_image(wav_path):
     S_dB = librosa.power_to_db(S, ref=np.max)
     S_norm = (S_dB + 80) / 80
 
-    resized = cv2.resize(S_norm, (224, 224), interpolation=cv2.INTER_CUBIC)
-    image = np.stack([resized, resized, resized], axis=-1)
-    image = (image * 255).astype(np.uint8)
-    return Image.fromarray(image)
+    # สร้างภาพ (H x W)
+    image = (S_norm * 255).astype(np.uint8)
+    pil_img = Image.fromarray(image).convert("RGB")
+
+    # Resize ด้วย Pillow ไม่ง้อ cv2
+    pil_img = pil_img.resize((224, 224), Image.BICUBIC)
+
+    return pil_img
