2125c3101b10d2977dec4c58a037fa97d2b406c7
Natawin Techawatcharapaikul <107763293+Natawin@users.noreply.github.com>
2025-06-12 14:26:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,6 @@ else:
             prob = torch.sigmoid(output).item()
 
         if prob > 0.5:
-            st.error(f"normal")
+            st.error(f"❌ Abnormal")
         else:
             st.success(f"✅ Normal")
